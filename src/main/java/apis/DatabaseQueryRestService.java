4998ceed7f2ad1e0c2388d8ea6a8e1421bbeb277
Ben <bpschreiber2003@gmail.com>
2020-10-30 13:45:57 -0700

    fixed the queries, added a constant so i didnt have to type a ton, added/modified existing queries to no longer reveal answers unless specifically requested.

--- src/main/java/apis/DatabaseQueryRestService.java
+++ src/main/java/apis/DatabaseQueryRestService.java
@@ -1,6 +1,7 @@
 package apis;
 
 import database.QueryExecutor;
+import etc.Constants;
 import org.json.JSONObject;
 
 import javax.ws.rs.GET;
@@ -18,8 +19,7 @@ public class DatabaseQueryRestService extends RestService {
     public Response getQuestions() {
         try {
             return okJSON(Response.Status.ACCEPTED, QueryExecutor.runQuery("select * from question").toString());
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             JSONObject errorJson = new JSONObject();
             errorJson.put("msg", "failure whale");
             return okJSON(Response.Status.ACCEPTED, errorJson.toString());
@@ -31,9 +31,8 @@ public class DatabaseQueryRestService extends RestService {
     @Path("questions/type/{type}")
     public Response getQuestionsByType(@PathParam("type") String type) {
         try {
-            return okJSON(Response.Status.ACCEPTED, QueryExecutor.runQuery("select * from question, _type where question.type_id=_type.id and _type.type_name='" + type + "'" ).toString());
-        }
-        catch (Exception e) {
+            return okJSON(Response.Status.ACCEPTED, QueryExecutor.runQuery(Constants.NOANSWERQUERY + ",_type where question.type_id=_type.id and _type.type_name='" + type + "'").toString());
+        } catch (Exception e) {
             JSONObject errorJson = new JSONObject();
             errorJson.put("msg", "failure whale");
             return okJSON(Response.Status.ACCEPTED, errorJson.toString());
@@ -45,9 +44,33 @@ public class DatabaseQueryRestService extends RestService {
     @Path("questions/subject/{subject}")
     public Response getQuestionsBySubject(@PathParam("subject") String subject) {
         try {
-            return okJSON(Response.Status.ACCEPTED, QueryExecutor.runQuery("select * from question, _type where question.type_id=_type.id and _type.type_name='" + subject + "'" ).toString());
+            return okJSON(Response.Status.ACCEPTED, QueryExecutor.runQuery(Constants.NOANSWERQUERY + ",_subject where question.subjects=_subject.id and _subject.subject_name='" + subject + "'").toString());
+        } catch (Exception e) {
+            JSONObject errorJson = new JSONObject();
+            errorJson.put("msg", "failure whale");
+            return okJSON(Response.Status.ACCEPTED, errorJson.toString());
         }
-        catch (Exception e) {
+    }
+
+    @GET
+    @Path("questions/{id}")
+    public Response getQuestionByID(@PathParam("id") String id) {
+        try {
+            return okJSON(Response.Status.ACCEPTED, QueryExecutor.runQuery(Constants.NOANSWERQUERY + "where question_num='" + id + "'").toString());
+        } catch (Exception e) {
+            JSONObject errorJson = new JSONObject();
+            errorJson.put("msg", "failure whale");
+            return okJSON(Response.Status.ACCEPTED, errorJson.toString());
+        }
+
+    }
+
+    @GET
+    @Path("questions/answer/{id}")
+    public Response getQuestionAnswerByID(@PathParam("id") String id) {
+        try {
+            return okJSON(Response.Status.ACCEPTED, QueryExecutor.runQuery("select answer from question where question_num='" + id + "'").toString());
+        } catch (Exception e) {
             JSONObject errorJson = new JSONObject();
             errorJson.put("msg", "failure whale");
             return okJSON(Response.Status.ACCEPTED, errorJson.toString());
